1f03c38f5ed4b6cdf342899b8d52ca4886adbb4c
Kyle Johnsen <kjohnsen@uga.edu>
2022-08-24 13:30:58 -0400

    skewed transform

---
+++ Assets/PrintMatrix.cs
@@ -0,0 +1,17 @@
+
+using UnityEngine;
+
+public class PrintMatrix : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(transform.localToWorldMatrix);
+    }
+}
