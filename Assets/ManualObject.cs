f22c951bd23d50c0e5744dde00bc509ce728de27
Kyle Johnsen <kjohnsen@uga.edu>
2022-08-30 14:01:00 -0400

    shaders and manual objects

---
+++ Assets/ManualObject.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualObject : MonoBehaviour
+{
+    public Material myMaterial;
+    Mesh mesh;
+    MeshRenderer renderer;
+    MeshFilter filter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        Vector3[] verts = new Vector3[3];
+        verts[0] = new Vector3(0, 0, 0);
+        verts[1] = new Vector3(1, 0, 0);
+        verts[2] = new Vector3(1, 1, 0);
+
+        int[] inds = new int[] { 0, 1, 2, 2, 1, 0 };
+
+        mesh.vertices = verts;
+        mesh.SetIndices(inds, MeshTopology.Triangles, 0);
+
+        renderer = gameObject.AddComponent<MeshRenderer>();
+        renderer.material = myMaterial;
+        filter = gameObject.AddComponent<MeshFilter>();
+        filter.mesh = mesh;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/PrintMatrix.cs
+++ Assets/PrintMatrix.cs
@@ -1,8 +1,9 @@
 
 using UnityEngine;
-
+using UnityEngine.UI;
 public class PrintMatrix : MonoBehaviour
 {
+    public Text screenText;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +13,16 @@ public class PrintMatrix : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(screenText != null)
+		{
+            screenText.text = ""+this.GetComponent<Camera>().projectionMatrix;
+
+        }
+        if(this.GetComponent<Camera>() != null)
+		{
+            Debug.Log(this.GetComponent<Camera>().projectionMatrix);
+		}
         Debug.Log(transform.localToWorldMatrix);
+
     }
 }
